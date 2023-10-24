9e24c0b128c8e74309f25e50f505f9a5df6e8333
Scott Fitzgerald <scott@droolcup.com>
2023-10-24 09:59:39 -0400

    Update sketch.js

--- APIs/cors-proxy-example/sketch.js
+++ APIs/cors-proxy-example/sketch.js
@@ -1,5 +1,15 @@
+// let url = "https://corsproxy.io/?http://api.open-notify.org/astros.json";
+//let url = "http://api.open-notify.org/astros.json";
+
+let data;
+
+function preload(){
+  data = loadJSON(url);
+} 
+
 function setup() {
   createCanvas(400, 400);
+  console.log(data);
 }
 
 function draw() {
